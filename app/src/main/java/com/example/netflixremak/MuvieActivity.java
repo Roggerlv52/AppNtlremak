9adf8b18ad6894d2716b151a85b50ea376ab5bcb
Roger <roggerlvr52@gmail.com>
2021-12-29 17:12:09 -0300

    *Customisando activity_muvie *Criando playCirculo * Criando metodo para trocar imagem dinamicamente *Criando um shadows xml.

--- app/src/main/java/com/example/netflixremak/MuvieActivity.java
+++ app/src/main/java/com/example/netflixremak/MuvieActivity.java
@@ -2,8 +2,11 @@ package com.example.netflixremak;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.core.content.ContextCompat;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MuvieActivity extends AppCompatActivity {
 
@@ -18,5 +21,14 @@ public class MuvieActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
         }
+        //Metodo para trocar imagem dinamicamente
+      LayerDrawable drawable = (LayerDrawable) ContextCompat.getDrawable(this,R.drawable.shadows);
+        if (drawable != null){
+          Drawable movierCover = ContextCompat.getDrawable(this,R.drawable.movie_4
+          );
+          drawable.setDrawableByLayerId(R.id.coverDlawable,movierCover);
+            ((ImageView)findViewById(R.id.image_couver_View)).setImageDrawable(drawable);
+        }
+
     }
 }
